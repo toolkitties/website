9f8dc682ca9db300cd322fe28b24eaad991ccc78
adz <x12@adz.garden>
2025-03-20 15:15:59 +0100

    Cute and more accessible

--- assets/app.js
+++ assets/app.js
@@ -1,28 +1,38 @@
+const MOBILE_WIDTH = 1024;
+
 const scrollContainer = document.getElementById("scroll");
 const currentSection = document.getElementById("current-section");
 const headings = ["h1", "h2", "h3", "h4", "h5", "h6"].reduce(
   (acc, headingTag) => {
-    return acc.concat([...document.querySelectorAll(`main ${headingTag}`)]);
+    return acc.concat([...document.querySelectorAll(headingTag)]);
   },
   [],
 );
+
+function getScrollContainer() {
+  if (window.innerWidth <= MOBILE_WIDTH) {
+    return document.documentElement;
+  } else {
+    return scrollContainer;
+  }
+}
 
 // Change title based on where user scrolled to.
 
 const defaultTitle = currentSection.innerText;
 
 function checkScrollPosition() {
+  const target = getScrollContainer();
   currentSection.innerText = defaultTitle;
   for (const heading of headings) {
-    if (scrollContainer.scrollTop > heading.offsetTop - heading.offsetHeight - 1) {
+    if (target.scrollTop > heading.offsetTop - heading.offsetHeight - 1) {
       currentSection.innerText = heading.innerText;
     }
   }
-
-  console.log(scrollContainer.offsetHeight, scrollContainer.scrollTop);
 }
 
 scrollContainer.addEventListener("scroll", checkScrollPosition);
+document.addEventListener("scroll", checkScrollPosition);
 document.addEventListener("resize", checkScrollPosition);
 
 // Automatically render the "table of contents" into the menu.
@@ -31,11 +41,20 @@ const nav = document.getElementById("navigation");
 const navItems = document.getElementById("navigation-items");
 
 for (const heading of headings) {
+  if (("aria-hidden" in heading.attributes)) {
+    continue;
+  }
+
   const menu = document.createElement("li");
-  menu.innerText = heading.innerText;
+  const anchor = document.createElement("a");
+  anchor.href = "#";
+  menu.appendChild(anchor);
+  anchor.innerText = heading.innerText;
   menu.addEventListener("click", (event) => {
+    event.preventDefault();
     event.stopPropagation();
-    scrollContainer.scrollTo({
+    const target = getScrollContainer();
+    target.scrollTo({
       behavior: "smooth",
       top: heading.offsetTop - heading.offsetHeight,
     });
